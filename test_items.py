d37bb296abdd6d1afdde6dbbc6c067e981773009
iLnazhim <bass_boss@mail.ru>
2022-06-17 14:56:17 +0300

    Второй день практики

--- test_items.py
+++ test_items.py
@@ -8,26 +8,4 @@ def test_check_language(browser):
     browser.get(link)
     assert browser.find_element(By.CSS_SELECTOR, "button.btn-add-to-basket"), "Кнопка не найдена"
     time.sleep(10)
-    # assert a == "button.btn-add-to-basket"
-
-    # чтобы
-    # заработало
-    # надо
-    # использовать
-    # find_elementS
-    # 
-    # т.к.данная
-    # конструкция
-    # не
-    # дает
-    # тесту
-    # просто
-    # упасть, а
-    # возвращает
-    # пустое
-    # множество, что
-    # позволяет
-    # сработать
-    # ассерту
-    # 
-    # assert browser.find_elements_by_css_selector("***"), 'basket button not found'
+  
