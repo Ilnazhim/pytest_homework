8d206e1eec8f247999f68eb25155cd2e81b3e41a
iLnazhim <bass_boss@mail.ru>
2022-06-17 14:17:36 +0300

    Изменение языка финал

--- conftest.py
+++ conftest.py
@@ -2,12 +2,14 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
+
 def pytest_addoption(parser):
-    parser.addoption('--browser_name', action='store', default="chrome",
+    parser.addoption('--browser_name', action='store', default='chrome',
                      help="Choose browser: chrome or firefox")
-def pytest_addoption(parser):
     parser.addoption('--language', action='store', default=None,
                      help="Choose language: es or fr")
+
+
 
 @pytest.fixture(scope="function")
 def browser(request):
--- test_items.py
+++ test_items.py
@@ -1,11 +1,14 @@
 from selenium.webdriver.common.by import By
+import time
+
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-def test_find_button(browser):
+def test_check_language(browser):
     browser.get(link)
-    a = browser.find_elements(By.CSS_SELECTOR, "button.btn-add-to-basket")
-    assert a == "button.btn-add-to-basket"
+    assert browser.find_element(By.CSS_SELECTOR, "button.btn-add-to-basket"), "Кнопка не найдена"
+    time.sleep(10)
+    # assert a == "button.btn-add-to-basket"
 
     # чтобы
     # заработало
